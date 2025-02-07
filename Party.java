7703fef3c162f87560ab1eab475c908e42beabe3
Beyerbach <beyerdan27@AD.7HILLS.ORG>
2025-02-07 10:56:33 -0500

    visual studio code can stop autocompleting my code thank you

--- Party.java
+++ Party.java
@@ -13,8 +13,10 @@ public class Party{
 	//instance variables as the kids say nowadays
 	//private String[] temp = new String[2];
 	private int numTables, numSeats;
-	ArrayList<String> companyIDs = new ArrayList<String>();
-	ArrayList<ArrayList<String>> guests = new ArrayList<ArrayList<String>>();
+	private ArrayList<String> companyIDs = new ArrayList<String>();
+	private ArrayList<ArrayList<String>> guests = new ArrayList<ArrayList<String>>();
+	private ArrayList<Integer> numGuestsPerCompany = new ArrayList<Integer>();
+	numGuestsPerCompany.set(0, "-1");
 	public Party(int numTablesA, int numSeatsA){ //constructing the party, like it's a party planner
 		numTables = numTablesA;
 		numSeats = numSeatsA;
@@ -24,23 +26,53 @@ public class Party{
 			File companies = new File("companies.txt");
 			Scanner reader1 = new Scanner(companies);
 			while(reader1.hasNextLine()){
-				String[] temp = reader1.nextLine().split(",");
-				System.out.println(temp[0]);
+				String temp = reader1.nextLine();
+				if(temp.length()>0){
+					companyIDs.add(temp.substring(3));
+				}
 			}
+			reader1.close();
 		}
 		catch(FileNotFoundException e){
 			return 404;
 		}
-		/*try{
-			
+		try{
+			File guestss = new File("partyguests.txt");
+			Scanner reader2 = new Scanner(guestss);
+			while(reader2.hasNextLine()){
+				String[] temp = reader2.nextLine().split(",");
+				if(temp.length>0){
+					ArrayList<String> templist = new ArrayList<String>();
+					for(String t:temp) templist.add(t);
+					guests.add(templist);
+				}
+			}
+			reader2.close();
 		}
 		catch(FileNotFoundException e){
 			return 404;
+		}
+		/*(for(ArrayList<String> templistt:guests){
+			for(String s:templistt){
+			System.out.print(s+" ");
+		}
+		System.out.println();
 		}*/
 		return 200;
-			
-		
-		
-		
 	}
+	public ArrayList<Integer> enumerateGuests(){ //returns list of companies that exceed attendance limits
+		String currentCompanyIDBeingCounted = guests.get(0).get(3);
+		int currentCount=0;
+		for(ArrayList<String> templist:guests){
+			if(templist.get(3).equals(currentCompanyIDBeingCounted)){
+				currentCount++;
+				numGuestsPerCompany.set(currentCompanyIDBeingCounted, currentCount);
+			} else {
+				currentCount=1;
+				currentCompanyIDBeingCounted = templist.get(3);
+			}
+		}//INCOMPLETE
+			
+	}
+	
 }
